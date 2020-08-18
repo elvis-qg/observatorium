7c1f4c8fa44cc84f49b7774bb4bbc011e5cab25b
ElvisQG <elvis.sqg@gmail.com>
2020-08-18 11:38:13 -0500

    adding /medicine route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./features/home/Home";
+import Medicine from "./features/medicine/medicine";
 
 export default function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/medicine" exact component={Medicine} />
       </Switch>
     </Router>
   );
